e09f99675f135f48706c7f062c979c2f9c646b3c
MohanVaddella <mohanvaddella@gmail.com>
2024-02-02 07:16:39 +0530

    changed name for logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
             <img
               src="https://flowbite.com/docs/images/logo.svg"
               className="mr-3 h-6 sm:h-9"
-              alt="Flowbite Logo"
+              alt="Vidgalaxy Logo"
             />
             <span className="self-center text-xl font-semibold whitespace-nowrap text-white dark:text-white">
             VidGalaxy
--- frontend/src/components/HeaderOne.jsx
+++ frontend/src/components/HeaderOne.jsx
@@ -47,7 +47,7 @@ const HeaderOne = () => {
             <img
               src="https://flowbite.com/docs/images/logo.svg"
               className="mr-3 h-6 sm:h-9"
-              alt="Flowbite Logo"
+              alt="Vidgalaxy Logo"
             />
             <span className="self-center text-xl font-semibold whitespace-nowrap text-white dark:text-white">
             VidGalaxy
